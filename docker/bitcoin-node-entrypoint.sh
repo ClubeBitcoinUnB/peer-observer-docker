0fb748c38768474dfad05077c2edbe547295fcf8
Maycon Fabio <m.vinnicyus@gmail.com>
2025-07-18 22:59:07 -0300

    Remove bitcoin-bin volume

--- docker/bitcoin-node-entrypoint.sh
+++ docker/bitcoin-node-entrypoint.sh
@@ -30,21 +30,21 @@ esac
 
 # Start bitcoind as bitcoin user, in the background
 echo "Launching Bitcoin node in $BITCOIN_NETWORK mode..."
-/usr/sbin/runuser -u bitcoin -- /shared/bitcoind $NETWORK &
+/usr/sbin/runuser -u bitcoin -- $BTC_BIN_PATH/bitcoind $NETWORK &
 BITCOIND_PID=$!
 
 # Now wait for the RPC
 for i in {1..30}; do
-    /usr/sbin/runuser -u bitcoin -- /shared/bitcoin-cli $NETWORK getblockchaininfo >/dev/null 2>&1 && break
+    /usr/sbin/runuser -u bitcoin -- $BTC_BIN_PATH/bitcoin-cli $NETWORK getblockchaininfo >/dev/null 2>&1 && break
     sleep 1
 done
 
 # Final check
-if ! /usr/sbin/runuser -u bitcoin -- /shared/bitcoin-cli $NETWORK getblockchaininfo >/dev/null 2>&1; then
+if ! /usr/sbin/runuser -u bitcoin -- $BTC_BIN_PATH/bitcoin-cli $NETWORK getblockchaininfo >/dev/null 2>&1; then
     echo "Error: bitcoind did not start in time." >&2
     exit 1
 fi
 
 echo "Starting ebpf-extractor"
 # Run ebpf-extractor as root (needs CAP_SYS_ADMIN for BPF)
-exec /usr/local/bin/ebpf-extractor --no-idle-exit --nats-address nats://nats:4222 --bitcoind-path /shared/bitcoind
+exec /usr/local/bin/ebpf-extractor --no-idle-exit --nats-address nats://nats:4222 --bitcoind-path $BTC_BIN_PATH/bitcoind
